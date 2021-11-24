5dc31e5080d95f32545eada8a5bd5f1179c4de8f
andreanpradanaa <andreanpradana2001@gmail.com>
2021-11-24 17:11:27 +0700

    commit

--- src/components/imports/Navbar.js
+++ src/components/imports/Navbar.js
@@ -1,38 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { logoutAction } from "../../container/actions";
 import { useDispatch } from "react-redux";
 import Logout from "../imports/Logout";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import Button from "react-bootstrap/Button";
 
 function Navbar() {
   const dispatch = useDispatch();
   const logout = () => {
     dispatch(logoutAction());
   };
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark fixed-top">
       <a className="navbar-brand" href="/">
         Admin
       </a>
 
-      <form className="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-        <div className="input-group">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Search for..."
-            aria-label="Search"
-            aria-describedby="basic-addon2"
-          />
-          <div className="input-group-append">
-            <button className="btn btn-primary" type="button">
-              <i className="fas fa-search"></i>
-            </button>
-          </div>
-        </div>
-      </form>
+      {/* <button
+        class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0"
+        id="sidebarToggle"
+      >
+        <i class="fas fa-bars"></i>
+      </button> */}
 
-      <ul className="navbar-nav ml-auto ml-md-0">
+      <div className="d-none d-md-inline-block ml-auto mr-0 mr-md-3 my-2 my-md-0"></div>
+
+      <ul className="navbar-nav ml-auto ml-md-0 ">
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            id="userDropdown"
+            href="/#"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <i class="fas fa-bars"></i>
+          </a>
+          <div
+            className="dropdown-menu dropdown-menu-right"
+            aria-labelledby="userDropdown"
+          >
+            <Link className="dropdown-item text-center" to={{ pathname: `/` }}>
+              Dashboard
+            </Link>
+            <Link
+              className="dropdown-item text-center"
+              to={{ pathname: `/clients` }}
+            >
+              Clients
+            </Link>
+            <Link
+              className="dropdown-item text-center"
+              to={{ pathname: `/teams` }}
+            >
+              Teams
+            </Link>
+            <Link
+              className="dropdown-item text-center"
+              to={{ pathname: `/projects` }}
+            >
+              Projects
+            </Link>
+          </div>
+        </li>
+      </ul>
+
+      <ul className="navbar-nav ">
         <li className="nav-item dropdown">
           <a
             className="nav-link dropdown-toggle"
